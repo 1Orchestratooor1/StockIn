1791476baa490bafb2cb3be58eb66dcabebadc4d
1Orchestratooor1 <rodriguezjhon2090@gmail.com>
2024-12-05 23:02:08 -0500

    vite config js commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',  // Especifica que la salida de la construcción sea en el directorio 'dist'
+  },
 })
